75fd08422df5c397759a1ce9a268a59a16a2f6b9
Mateusz Kojro <mateuszkojro@outloook.com>
2020-12-19 15:27:27 +0100

    clean up tests

--- test/test_obraz.cpp
+++ test/test_obraz.cpp
@@ -2,12 +2,6 @@
 #include "ppm.h"
 #include <catch2/catch.hpp>
 
-// TEST_CASE("counting simple colors", "[obraz]")
-// {
-//     ppm p("../test/test_files/only_pixels.ppm");
-//     REQUIRE(p.count_colors() == 3);
-// }
-
 TEST_CASE("counting simple colors FULL", "[obraz]") {
   ppm p("../test/test_files/pixels_full.ppm");
   REQUIRE(p.count_colors() == 9);
@@ -17,18 +11,6 @@ TEST_CASE("counting simple pixels FULL", "[obraz]") {
   ppm p("../test/test_files/pixels_full.ppm");
   REQUIRE(p.count_pixels() == 9);
 }
-
-// TEST_CASE("count colors in lena", "[obraz]")
-// {
-//     ppm p("../test/test_files/lena.ppm");
-//     REQUIRE(p.count_colors() == 52669);
-// }
-
-// TEST_CASE("count pixels in lena", "[obraz]")
-// {
-//     ppm p("../test/test_files/lena.ppm");
-//     REQUIRE(p.count_pixels() == 512 * 512);
-// }
 
 TEST_CASE("count pixels in full lena", "[obraz]") {
   ppm p("../test/test_files/lena_full.ppm");
